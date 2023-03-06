4504b570d9ab394f38c2ea01aad643630520b710
realcryptonight <me@realcryptonight.dev>
2023-03-06 13:11:25 +0100

    Changed: FTP scripts to only use S3 and stripped out anything that is not needed

--- files/ftp_list.php
+++ files/ftp_list.php
@@ -1,88 +1,20 @@
 #!/bin/bash
 
-CURL=/usr/local/bin/curl
-if [ ! -e ${CURL} ]; then
-	CURL=/usr/bin/curl
-fi
+# DirectAdmin download script.
+# Modified by realcryptonight.
+
 RCLONE=/usr/bin/rclone
 TMPDIR=/home/tmp
-PORT=${ftp_port}
-FTPS=0
-if [ "${ftp_secure}" = "ftps" ]; then
-	FTPS=1
-fi
-SHOW_BYTES=0
-if [ "${ftp_show_bytes}" = "yes" ]; then
-	SHOW_BYTES=1
-fi
-SSL_REQD=""
-if ${CURL} --help tls | grep -m1 -q 'ftp-ssl-reqd'; then
-    SSL_REQD="--ftp-ssl-reqd"
-elif ${CURL} --help tls | grep -m1 -q 'ssl-reqd'; then
-    SSL_REQD="--ssl-reqd"
-fi
-
-if [ "$FTPS" = "0" ]; then
-	SSL_REQD=
-fi
-
-if [ "$PORT" = "" ]; then
-	PORT=21
-fi
-
 RANDNUM=`/usr/local/bin/php -r 'echo rand(0,10000);'`
 #we need some level of uniqueness, this is an unlikely fallback.
 if [ "$RANDNUM" = "" ]; then
         RANDNUM=$ftp_ip;
 fi
 
-CFG=$TMPDIR/$RANDNUM.cfg
-rm -f $CFG
-touch $CFG
-chmod 600 $CFG
-
 DUMP=$TMPDIR/$RANDNUM.dump
 rm -f $DUMP
 touch $DUMP
 chmod 600 $DUMP
-
-#######################################################
-# FTP
-list_files()
-{
-	if [ ! -e ${CURL} ]; then
-		echo "";
-		echo "*** Unable to get list ***";
-		echo "Please install curl by running:";
-		echo "";
-		echo "cd /usr/local/directadmin/custombuild";
-		echo "./build curl";
-		echo "";
-		exit 10;
-	fi
-
-	#double leading slash required, because the first one doesn't count.
-	#2nd leading slash makes the path absolute, in case the login is not chrooted.
-	#without double forward slashes, the path is relative to the login location, which might not be correct.
-	ftp_path="/${ftp_path}"
-
-	/bin/echo "user =  \"$ftp_username:$ftp_password_esc_double_quote\"" >> $CFG
-
-	${CURL} --config ${CFG} ${SSL_REQD} -k --silent --show-error ftp://$ftp_ip:${PORT}$ftp_path/ > ${DUMP} 2>&1
-	RET=$?
-
-	if [ "$RET" -ne 0 ]; then
-		echo "${CURL} returned error code $RET";
-		cat $DUMP
-	else
-		COLS=`awk '{print NF; exit}' $DUMP`
-		if [ "${SHOW_BYTES}" = "1" ] && [ "${COLS}" = "9" ]; then
-			cat $DUMP | grep -v -e '^d' | awk "{ print \$${COLS} \"=\" \$5; }"
-		else
-			cat $DUMP | grep -v -e '^d' | awk "{ print \$${COLS}; }"
-		fi
-	fi
-}
 
 #######################################################
 # Object Storage via rclone
@@ -98,8 +30,7 @@ list_object_storage()
 		exit 10;
 	fi
 	
-	#rclone command here
-	rclone --config "/home/${ftp_password_esc_double_quote}/.config/rclone/rclone.conf" lsf --format "p" --files-only $ftp_username$ftp_path
+	$RCLONE --config "/home/${ftp_password_esc_double_quote}/.config/rclone/rclone.conf" lsf --format "p" --files-only $ftp_username$ftp_path
 	RET=$?
 
 	if [ "$RET" -ne 0 ]; then
@@ -113,13 +44,8 @@ list_object_storage()
 #######################################################
 # Start
 
-if [ "${PORT}" = "443" ]; then
-	list_object_storage
-else
-	list_files
-fi
+list_object_storage
 
-rm -f $CFG
 rm -f $DUMP
 
 exit $RET
--- files/ftp_upload.php
+++ files/ftp_upload.php
@@ -1,63 +1,13 @@
 #!/bin/bash
 
-# Original script created by: DirectAdmin
-# Modified by: realcryptonight
+# DirectAdmin download script.
+# Modified by realcryptonight.
 
-
-VERSION=1.2
-CURL=/usr/local/bin/curl
-if [ ! -e ${CURL} ]; then
-		CURL=/usr/bin/curl
-fi
 RCLONE=/usr/bin/rclone
-DU=/usr/bin/du
-BC=/usr/bin/bc
-EXPR=/usr/bin/expr
-TOUCH=/bin/touch
-PORT=${ftp_port}
-FTPS=0
-
 MD5=${ftp_md5}
-
-if [ "${ftp_secure}" = "ftps" ]; then
-	FTPS=1
-fi
-
-CURL_TLS_HELP=$(${CURL} --help tls)
-CURL_VERSION=$(${CURL} --version | head -n 1 | cut -d ' ' -f 2)
-int_version() {
-	local major minor patch
-	major=$(cut -d . -f 1 <<< "$1")
-	minor=$(cut -d . -f 2 <<< "$1")
-	patch=$(cut -d . -f 3 <<< "$1")
-	printf "%03d%03d%03d" "${major}" "${minor}" "${patch}"
-}
-
-SSL_ARGS=""
-if grep -q 'ftp-ssl-reqd' <<< "${CURL_TLS_HELP}"; then
-    SSL_ARGS="${SSL_ARGS} --ftp-ssl-reqd"
-elif grep -q 'ssl-reqd' <<< "${CURL_TLS_HELP}"; then
-    SSL_ARGS="${SSL_ARGS} --ssl-reqd"
-fi
-if grep -q 'Use TLSv1.1 or greater' <<< "${CURL_TLS_HELP}"; then
-	SSL_ARGS="${SSL_ARGS} --tlsv1.1"
-fi
-
-# curl 7.78.0 fixed FTP upload TLS 1.3 bug, we add `--tls-max 1.2` for older
-# versions.
-if [ "$(int_version "${CURL_VERSION}")" -lt "$(int_version '7.78.0')" ] && grep -q 'tls-max' <<< "${CURL_TLS_HELP}"; then
-	SSL_ARGS="${SSL_ARGS} --tls-max 1.2"
-fi
 
 #######################################################
 # SETUP
-
-if [ ! -e $TOUCH ] && [ -e /usr/bin/touch ]; then
-	TOUCH=/usr/bin/touch
-fi
-if [ ! -x ${EXPR} ] && [ -x /bin/expr ]; then
-	EXPR=/bin/expr
-fi
 
 if [ ! -e "${ftp_local_file}" ]; then
 	echo "Cannot find backup file ${ftp_local_file} to upload";
@@ -88,92 +38,11 @@ get_md5() {
 
 #######################################################
 
-CFG=${ftp_local_file}.cfg
-/bin/rm -f $CFG
-$TOUCH $CFG
-/bin/chmod 600 $CFG
-
 RET=0;
 
 #######################################################
-# FTP
-upload_file_ftp()
-{
-        if [ ! -e ${CURL} ]; then
-                echo "";
-                echo "*** Backup not uploaded ***";
-                echo "Please install curl by running:";
-                echo "";
-                echo "cd /usr/local/directadmin/custombuild";
-                echo "./build curl";
-                echo "";
-                exit 10;
-        fi
-
-        /bin/echo "user =  \"$ftp_username:$ftp_password_esc_double_quote\"" >> $CFG
-
-        if [ ! -s ${CFG} ]; then
-                echo "${CFG} is empty. curl is not going to be happy about it.";
-                ls -la ${CFG}
-                ls -la ${ftp_local_file}
-                df -h
-        fi
-
-        #ensure ftp_path ends with /
-        ENDS_WITH_SLASH=`echo "$ftp_path" | grep -c '/$'`
-        if [ "${ENDS_WITH_SLASH}" -eq 0 ]; then
-                ftp_path=${ftp_path}/
-        fi
-
-        ${CURL} --config ${CFG} --silent --show-error --ftp-create-dirs --upload-file $ftp_local_file  ftp://$ftp_ip:${PORT}/$ftp_path$ftp_remote_file 2>&1
-        RET=$?
-
-        if [ "${RET}" -ne 0 ]; then
-                echo "curl return code: $RET";
-        fi
-}
-
-#######################################################
-# FTPS
-upload_file_ftps()
-{
-	if [ ! -e ${CURL} ]; then
-		echo "";
-		echo "*** Backup not uploaded ***";
-		echo "Please install curl by running:";
-		echo "";
-		echo "cd /usr/local/directadmin/custombuild";
-		echo "./build curl";
-		echo "";
-		exit 10;
-	fi
-
-	/bin/echo "user =  \"$ftp_username:$ftp_password_esc_double_quote\"" >> $CFG
-
-	if [ ! -s ${CFG} ]; then
-		echo "${CFG} is empty. curl is not going to be happy about it.";
-		ls -la ${CFG}
-		ls -la ${ftp_local_file}
-		df -h
-	fi
-
-	#ensure ftp_path ends with /
-	ENDS_WITH_SLASH=`echo "$ftp_path" | grep -c '/$'`
-	if [ "${ENDS_WITH_SLASH}" -eq 0 ]; then
-		ftp_path=${ftp_path}/
-	fi
-
-	${CURL} --config ${CFG} ${SSL_ARGS} -k --silent --show-error --ftp-create-dirs --upload-file $ftp_remote_file  ftp://$ftp_ip:${PORT}/$ftp_path$ftp_remote_file 2>&1
-	RET=$?
-
-	if [ "${RET}" -ne 0 ]; then
-		echo "curl return code: $RET";
-	fi
-}
-
-#######################################################
 # Object Storage via rsync
-upload_file_object_storage()
+upload_file_to_object_storage()
 {
 	if [ ! -e ${RCLONE} ]; then
 		echo "";
@@ -185,7 +54,7 @@ upload_file_object_storage()
 		exit 10;
 	fi
 	
-	rclone --config "/home/${ftp_password_esc_double_quote}/.config/rclone/rclone.conf" copy $ftp_local_file $ftp_username$ftp_path
+	$RCLONE --config "/home/${ftp_password_esc_double_quote}/.config/rclone/rclone.conf" copy $ftp_local_file $ftp_username$ftp_path
 	RET=$?
 	
 	if [ "${RET}" -ne 0 ]; then
@@ -196,17 +65,10 @@ upload_file_object_storage()
 #######################################################
 # Start
 
-if [ "${PORT}" = "443" ]; then
-	upload_file_object_storage
-else
-	if [ "${FTPS}" = "1" ]; then
-		upload_file_ftps
-	else
-		upload_file_ftp
-	fi
-fi
+upload_file_to_object_storage
 
 if [ "${RET}" = "0" ] && [ "${MD5}" = "1" ]; then
+	
 	MD5_FILE=${ftp_local_file}.md5
 	M=`get_md5 ${ftp_local_file}`
 	if [ "${M}" != "" ]; then
@@ -215,19 +77,9 @@ if [ "${RET}" = "0" ] && [ "${MD5}" = "1" ]; then
 		ftp_local_file=${MD5_FILE}
 		ftp_remote_file=${ftp_remote_file}.md5
 		
-		if [ "${PORT}" = "443" ]; then
-			upload_file_object_storage
-		else
-			if [ "${FTPS}" = "1" ]; then
-				upload_file_ftps
-			else
-				upload_file
-			fi
-		fi
+		upload_file_to_object_storage
 	fi
 fi
-
-/bin/rm -f $CFG
 
 exit $RET
 
