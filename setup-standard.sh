1160d5006b7b721b24488061f020f9b5a8a07f03
realcryptonight <me@realcryptonight.dev>
2024-12-01 23:03:37 +0100

    Removed: no ssh password auth from standard setup since this should be done bia cloud-init

--- setup-standard.sh
+++ setup-standard.sh
@@ -7,10 +7,4 @@ mv ./files/10-uname /etc/update-motd.d/
 mv ./files/90-footer /etc/update-motd.d/
 chmod 777 /etc/update-motd.d/*
 
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
-sed -i 's/PermitRootLogin yes/PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-service sshd restart
-
 exit 0
